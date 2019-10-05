d2399036d52cf6c7f5766f4c3401a33926068b48
Oluwafemi John Semako <33866480+Jeans212@users.noreply.github.com>
2019-10-05 20:04:42 +0100

    Updated entity_naming.py

--- entity_naming.py
+++ entity_naming.py
@@ -1,31 +1,60 @@
+"""
+This is the 'entity_naming' module.
+
+Named entities help us understand more about what is being
+referred to in a given text so that we can further classify the data.
+Since named entities comprise more than one word,
+it is sometimes difficult to find these from the text.
+
+"""
+
 # importing libraries
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
 from pprint import pprint
+import nltk
 import spacy
-import en_core_web_sm
 
 def preprocess(sent):
-    sent = nltk.word_tokenize(sent)
-    sent = nltk.pos_tag(sent)
+    """
+    >>> preprocess('European authorities fined Google on Wednesday')
+    List of words and their parts of speech tag:
+    [('European', 'JJ'),
+     ('authorities', 'NNS'),
+     ('fined', 'VBD'),
+     ('Google', 'NNP'),
+     ('on', 'IN'),
+     ('Wednesday', 'NNP')]
+     """
+    resent = nltk.word_tokenize(sent)
+    resent = nltk.pos_tag(resent)
     print("\nList of words and their parts of speech tag:")
-    pprint(sent)
+    pprint(resent)
 
-def entity_naming():
+def entity_naming(sentence):
+    """
+    >>> entity_naming('European authorities fined Google on Wednesday')
+
+    List of words and their entities:
+    [('European', 'NORP'), ('Google', 'ORG'), ('Wednesday', 'DATE')]
+
+    Explanation of Entities:
+    [('NORP', 'Nationalities or religious or political groups'),
+     ('', None),
+     ('', None),
+     ('ORG', 'Companies, agencies, institutions, etc.'),
+     ('', None),
+     ('DATE', 'Absolute or relative dates or periods')]
+     """
     # loading the language that would be recognized
     nlp = spacy.load("en_core_web_sm")
-    document = nlp(corpus)
-
+    document = nlp(sentence)
     #prints the recognized words and their types
     print("\nList of words and their entities:")
     pprint([(word.text, word.label_) for word in document.ents])
-    
     #prints the type of the word recognized and the meaning
     print("\nExplanation of Entities:")
     pprint([(word.ent_type_, spacy.explain(word.ent_type_)) for word in document])
 
-if __name__=='__main__':
-    corpus = input("Enter the sentence: ")
-    preprocess(corpus)
-    entity_naming()
+if __name__ == '__main__':
+    CORPUS = input("Enter the sentence: ")
+    preprocess(CORPUS)
+    entity_naming(CORPUS)
